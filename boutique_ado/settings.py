ea63ae3fbb5c01f58a8d65a8cbbaca2ffc9398bf
benamos216 <benamos216@gmail.com>
2023-01-03 14:15:18 +0000

    product admin - product form

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -176,6 +176,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECERT = os.getenv('STRIPER_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
